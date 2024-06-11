a147f7d51c057c47813a9bd5291f4db4b8171ac4
mzwang34 <mzwang34@163.com>
2024-06-11 20:39:32 +0800

    Revert "update README"
    This reverts commit 1bc6bb615834c5894204fc2882cbe5052a7c8eff.

---
+++ out/build/x64-Debug/CMakeFiles/smallRasterizer.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/Documents/vision/code/smallRasterizer/main.cpp" "CMakeFiles/smallRasterizer.dir/main.cpp.obj" "gcc" "CMakeFiles/smallRasterizer.dir/main.cpp.obj.d"
+  "D:/Documents/vision/code/smallRasterizer/model.cpp" "CMakeFiles/smallRasterizer.dir/model.cpp.obj" "gcc" "CMakeFiles/smallRasterizer.dir/model.cpp.obj.d"
+  "D:/Documents/vision/code/smallRasterizer/tgaimage.cpp" "CMakeFiles/smallRasterizer.dir/tgaimage.cpp.obj" "gcc" "CMakeFiles/smallRasterizer.dir/tgaimage.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ out/build/x64-Debug/CMakeFiles/smallRasterizer.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for smallRasterizer.
+# This may be replaced when dependencies are built.
